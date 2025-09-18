6bc168c0ab3b717dd2b605e996783017e69c47d6
badassdatascience <emw314159@gmail.com>
2025-09-17 22:59:00 -0700

    Loaded with initial values.

--- music_production_and_performance/theory/western/definitions.py
+++ music_production_and_performance/theory/western/definitions.py
@@ -1,3 +1,15 @@
 
+chromatic_scale_pitch_class_names = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B']
 
+enharmonics_map = {
+    'Cb' : 'B',
+    'C#' : 'Db',
+    'D#' : 'Eb',
+    'E#' : 'F',
+    'Fb' : 'E',
+    'Gb' : 'F#',
+    'G#' : 'Ab',
+    'A#' : 'Bb',
+    'B#' : 'C',
+}
     
