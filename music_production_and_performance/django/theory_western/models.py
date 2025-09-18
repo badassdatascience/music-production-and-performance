328be1e7d3535c283ef9546049c11ba3d18ed841
badassdatascience <emw314159@gmail.com>
2025-09-17 23:53:47 -0700

    Added a model representing interval fifth relationships between pitch classes.

--- music_production_and_performance/django/theory_western/models.py
+++ music_production_and_performance/django/theory_western/models.py
@@ -2,13 +2,24 @@ from django.db import models
 
 # Create your models here.
 
-
-#
-# need to add a uniqueness constraint
-#
 class PitchClass(models.Model):
     name = models.CharField(max_length = 200, unique = True)
     chromatic_index_base_c = models.IntegerField()
     enharmonics = models.ManyToManyField('self')
     comment = models.TextField(null = True)
 
+# need a uniqueness constraint
+class Fifths(models.Model):
+    pitch_class_below = models.ForeignKey(
+        PitchClass,
+        on_delete = models.CASCADE,
+        related_name = 'pitch_class_fifth_below',
+    )
+    
+    pitch_class = models.ForeignKey(PitchClass, on_delete = models.CASCADE)
+
+    pitch_class_above = models.ForeignKey(
+        PitchClass,
+        on_delete = models.CASCADE,
+        related_name = 'pitch_class_fifth_above',
+    )
