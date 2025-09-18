929652ed8a99328cb1fdce50f01d635811425d1a
badassdatascience <emw314159@gmail.com>
2025-09-18 10:20:59 -0700

    Added a path definition for general output files.

--- music_production_and_performance/config.py
+++ music_production_and_performance/config.py
@@ -5,5 +5,7 @@ config = {
 	    "ENGINE" : "django.db.backends.sqlite3",
 	    "NAME" : "/Users/emily/Desktop/projects/music-production-and-performance/music_production_and_performance/django/db.sqlite3"
 	}
-    }
+    },
+
+    'output_path' : '/Users/emily/Desktop/projects/music-production-and-performance/output'
 }
