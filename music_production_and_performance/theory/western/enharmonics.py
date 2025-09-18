f55b45f0c35ba34718c1cab12915cfce3c459062
badassdatascience <emw314159@gmail.com>
2025-09-18 11:09:36 -0700

    Added an alternative enharmonics formulation which I am considering.

--- music_production_and_performance/theory/western/enharmonics.py
+++ music_production_and_performance/theory/western/enharmonics.py
@@ -15,4 +15,26 @@ enharmonics_map = {
     'A#' : 'Bb',
     'B#' : 'C',
 }
-    
+
+#
+# define a C-based chromatic scale with its enharmonic spellings (up to two accidentals per spelling)                         
+#
+# We are not using this at the moment; keeping it around
+# while I decide whether this approach makes sense in any
+# way:
+#
+chromatic_c_based_enharmonic_spellings = [
+    ['Dbb', 'C', 'B#'],
+    ['Db', 'C#', 'B##'],
+    ['Ebb', 'D', 'C##'],
+    ['Fbb', 'Eb', 'D#'],
+    ['Fb', 'E', 'D##'],
+    ['Gbb', 'F', 'E#'],
+    ['Gb', 'F#', 'E##'],
+    ['Abb', 'G', 'F##'],
+    ['Ab', 'G#'],
+    ['Bbb', 'A', 'G##'],
+    ['Cbb', 'Bb', 'A#'],
+    ['Cb', 'B', 'A##'],
+]
+
