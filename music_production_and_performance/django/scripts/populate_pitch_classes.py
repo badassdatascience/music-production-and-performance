af5852785e2993dbde93646495296493a20b2568
badassdatascience <emw314159@gmail.com>
2025-09-18 13:29:19 -0700

    Moved the PitchClass loading code into the model.

--- music_production_and_performance/django/scripts/populate_pitch_classes.py
+++ music_production_and_performance/django/scripts/populate_pitch_classes.py
@@ -10,37 +10,16 @@ from theory_western.models import PitchClass
 from music_production_and_performance.theory.western.scales.chromatic import chromatic_scale_pitch_class_names
 from music_production_and_performance.theory.western.enharmonics import enharmonics_map
 
+#
+# initial load from a "pure"-ish chromatic scale
+#
+PitchClass.load_by_list_of_pitch_class_names(chromatic_scale_pitch_class_names)
 
-for i, pc_name in enumerate(chromatic_scale_pitch_class_names):
+#
+# now add the enharmonic spellings
+#
+PitchClass.load_enharmonics(enharmonics_map)
 
-    # need to add a specific exception for uniqueness violation
-    try:
-        pc = PitchClass(
-            name = pc_name.strip(),
-            chromatic_index_base_c = i,
-        )
-        pc.save()
-    except:
-        pass
-
-for name in enharmonics_map.keys():
-    enharmonic_name = enharmonics_map[name]
-    pc_base = PitchClass.objects.get(name = enharmonic_name)
-
-    # need to add a specific exception for uniqueness violation
-    try:
-        pc = PitchClass(
-            name = name.strip(),
-            chromatic_index_base_c = pc_base.chromatic_index_base_c,
-        )
-        pc.save()
-        pc.enharmonics.add(pc_base),
-        pc.save()
-    
-        pc_base.enharmonics.add(pc)
-        pc_base.save()
-    except:
-        pass
 
     
 
--- music_production_and_performance/django/theory_western/models.py
+++ music_production_and_performance/django/theory_western/models.py
@@ -10,6 +10,51 @@ class PitchClass(models.Model):
     camelot_major_number = models.IntegerField(null = True)
     comment = models.TextField(null = True)
 
+    #
+    # need to add a specific exception for uniqueness violation
+    #
+    # also, this is a static function; there might be a better design
+    # pattern available...
+    #
+    def load_by_list_of_pitch_class_names(chromatic_scale_pitch_class_names):
+        for i, pc_name in enumerate(chromatic_scale_pitch_class_names):
+            try:
+                pc = PitchClass(
+                    name = pc_name.strip(),
+                    chromatic_index_base_c = i,
+                )
+                pc.save()
+            except:
+                pass
+
+    #
+    # need to deal with specific exception for uniqueness violations
+    #
+    # also, this is a static function; there might be a better design
+    # pattern available...
+    #
+    def load_enharmonics(enharmonics_map):
+        for name in enharmonics_map.keys():
+            enharmonic_name = enharmonics_map[name]
+            pc_base = PitchClass.objects.get(name = enharmonic_name)
+
+            # need to add a specific exception for uniqueness violation
+            try:
+                pc = PitchClass(
+                    name = name.strip(),
+                    chromatic_index_base_c = pc_base.chromatic_index_base_c,
+                )
+                pc.save()
+                pc.enharmonics.add(pc_base),
+                pc.save()
+                
+                pc_base.enharmonics.add(pc)
+                pc_base.save()
+            except:
+                pass
+
+            
+    
 # need a uniqueness constraint
 class Fifths(models.Model):
     pitch_class_below = models.ForeignKey(
