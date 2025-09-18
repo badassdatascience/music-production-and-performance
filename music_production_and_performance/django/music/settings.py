2c234aeb6b5c68e1b0107e1fa479b00811cce2d3
badassdatascience <emw314159@gmail.com>
2025-09-17 22:51:59 -0700

    Getting the settings going.

--- music_production_and_performance/django/music/settings.py
+++ music_production_and_performance/django/music/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'theory_western.apps.TheoryWesternConfig',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
