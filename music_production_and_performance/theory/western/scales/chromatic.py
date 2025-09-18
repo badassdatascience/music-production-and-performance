de2405744d14415ae35e7632ff7baca761def34c
badassdatascience <emw314159@gmail.com>
2025-09-18 10:50:48 -0700

    Initial import.

---
+++ music_production_and_performance/theory/western/scales/chromatic.py
@@ -0,0 +1,26 @@
+
+#
+# While in reality the enumeration is arbitrary,
+# I want to explicitly control it by defining it
+# here:
+#
+chromatic_scale_pitch_class_names = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B']
+chromatic_scale_numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+#
+# Later more enharmonics will be added, or
+# maybe calculated to account for double-
+# and triple-accidentals:
+#
+enharmonics_map = {
+    'Cb' : 'B',
+    'C#' : 'Db',
+    'D#' : 'Eb',
+    'E#' : 'F',
+    'Fb' : 'E',
+    'Gb' : 'F#',
+    'G#' : 'Ab',
+    'A#' : 'Bb',
+    'B#' : 'C',
+}
+    
