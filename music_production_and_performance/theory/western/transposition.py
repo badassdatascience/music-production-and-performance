2c42463df623d6645be970f305f19426aedde159
Emily Williams <emily.marie.williams@protonmail.com>
2025-10-03 08:07:18 -0700

    Here is one way to express chromatic ideas.

--- music_production_and_performance/theory/western/transposition.py
+++ music_production_and_performance/theory/western/transposition.py
@@ -10,9 +10,10 @@ def chromatic_transpose(
     octaves = np.int16(np.floor(np.float16(pitches_as_numbers / 12.))) - 1
 
     if verbose:
-        print(notes_as_numbers)
-        print(pitches_as_numbers)
-        print(octaves)
+        print('Sequence provided by user:', notes_as_numbers)
+        print('Chromatic interval provided by user:', chromatic_interval)
+        print('Transposed pitch numbers:', pitches_as_numbers)
+        print('Corresponding octaves:', octaves)
         print()
     
     return pitches_as_numbers, octaves
